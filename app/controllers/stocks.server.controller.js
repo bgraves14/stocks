0b4e34dd720225a9d58627ec1d08099361694de4
Brandon Graves <bgraves14@gmail.com>
2015-11-23 17:17:10 +0000

    not working

--- app/controllers/stocks.server.controller.js
+++ app/controllers/stocks.server.controller.js
@@ -3,40 +3,95 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-    _ = require('lodash');
+ var mongoose = require('mongoose'),
+     errorHandler = require('./errors.server.controller'),
+     Stocks = mongoose.model('Stocks'),
+     _ = require('lodash');
 
 /**
  * Create a Stock
  */
-exports.create = function(req, res) {
+ exports.create = function(req, res) {
+     var stocks = new Stocks(req.body);
 
-};
+     stocks.save(function(err) {
+         if (err) {
+             return res.status(400).send({
+                 message: errorHandler.getErrorMessage(err)
+             });
+         } else {
+             res.status(201).json(stocks);
+         }
+     });
+ };
 
 /**
  * Show the current Stock
  */
-exports.read = function(req, res) {
-
-};
+ exports.read = function(req, res) {
+     Stocks.findById(req.params.stocksId).exec(function(err, stocks) {
+         if (err) {
+           return res.status(400).send({
+               message: errorHandler.getErrorMessage(err)
+           });
+       } else {
+          if (!stocks) {
+                 return res.status(404).send({
+                     message: 'Stock not found'
+                 });
+             }
+             res.json(stocks);
+       }
+     });
+ };
 
 /**
  * Update a Stock
  */
-exports.update = function(req, res) {
+ exports.update = function(req, res) {
+ 	var stocks = req.stocks;
 
-};
+ 	  stocks = _.extend(stocks, req.body);
+
+     	stocks.save(function(err) {
+     		if (err) {
+     		   return res.status(400).send({
+     		      message: errorHandler.getErrorMessage(err)
+     			});
+     		} else {
+     			res.json(stocks);
+     		}
+ 	});
+ };
 
 /**
  * Delete an Stock
  */
-exports.delete = function(req, res) {
+ exports.delete = function(req, res) {
+ 	var stocks = req.stocks;
 
-};
+   	stocks.remove(function(err) {
+   		if (err) {
+   		  return res.status(400).send({
+   				  message: errorHandler.getErrorMessage(err)
+   			});
+   		} else {
+   			res.json(stocks);
+   		}
+   	});
+ };
 
 /**
  * List of Stocks
  */
-exports.list = function(req, res) {
-
-};
+ exports.list = function(req, res) {
+     Stocks.find().exec(function(err, stocks) {
+         if (err) {
+             return res.status(400).send({
+                 message: errorHandler.getErrorMessage(err)
+             });
+         } else {
+             res.json(stocks);
+         }
+     });
+ };
--- app/routes/stocks.server.routes.js
+++ app/routes/stocks.server.routes.js
@@ -1,8 +1,21 @@
 'use strict';
 
 module.exports = function(app) {
-   app.route('/stocks')
-       .get(function (request, response) {
-           response.json([{ name: 'Beverages' }, { name: 'Condiments' }]);
-       });
+    var stocks = require('../../app/controllers/stocks.server.controller');
+
+    app.route('/stocks')
+      .get(stocks.list)
+      .post(stocks.create);
+
+      // the stocksId param is added to the params object for the request
+    app.route('/stocks/:stocksId')
+      .get(stocks.read)
+      .put(stocks.update)
+      .delete(stocks.delete);
+
+	// Finish by binding the article middleware
+	// What's this? Where the stocksID is present in the URL
+	// the logic to 'get by id' is handled by this single function
+	// and added to the request object i.e. request.stocks.
+	app.param('stocksId', stocks.stocksByID);
 };
